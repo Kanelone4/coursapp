8c19ccba492dda605fab65328973efe88aec5e86
Kanelone4 <chicoufarell05@gmail.com>
2024-12-03 03:47:13 +0100

    commit_first

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
-console.log("Code depuis la branche actuelle (master)");
-console.log("Code depuis la branche fusionnée (footer)");
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Content from './components/content';
+import Footer from './Components/footer/footer';
 
+function App() {
+  return (
+    <div className="App">
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/BlogPosts/blogPosts.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+
+const BlogPosts = () => {
+    return (
+
+            <div style={{position:'relative',justifyContent:'center',alignItems:'center',top:'20px',padding:'0px',left:'20px'}}>
+                <div>
+        </div>
+        <div>
+        <div className="mt-2 flex justify-between px-36" style={{ position: 'relative', left:'-64px',top:'-60px' }}>
+      <div className="relative w-1/3" style={{ position: 'relative', left:'20px' }}>
+        <img
+          src={'https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/hero01-free-img-1024x647.jpg'}
+          alt=""
+          className="w-full h-64 object-cover"
+          style={{ position: 'relative', top: '-25px',left: '40px' }}
+        />
+        <p style={{ left: '40px', top: '-8px', position: 'relative', fontSize: '21px', fontFamily: 'sans-serif', fontWeight: 'bold' }}>
+        A Lesson From the Henrietta Lacks Story: Science Needs Your Cells
+          </p>
+          
+          <p style={{ position: 'relative', left: '40px', top: '22px', fontSize: '16px', fontFamily: 'sans-serif' }} >
+            Cursus iaculis etiam in In nullam donec sem sed <br /> consequat scelerisque nibh ...
+          </p>
+        <div
+          style={{ position: 'relative', left: '40px', top: '-50px', fontSize: '13px', fontFamily: 'sans-serif' }}
+        >
+          <span >akbarh   </span>
+          <span>  July 7, 2021</span>
+        </div>
+      </div>
+
+      <div className="relative w-1/3 mx-4">
+     
+        <img
+          src={'https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/pic20-free-img.jpg'}
+          alt=""
+          className="w-full h-64 object-cover"
+          style={{ position: 'relative', top: '-25px' }}
+        />
+          <p style={{ left: '10px', top: '-8px', position: 'relative', fontSize: '21px', fontFamily: 'sans-serif', fontWeight: 'bold' }}>
+          What Moves Gravel-Size Gypsum Crystals Around the Desert?
+          </p>
+          
+          <p style={{ position: 'relative', left: '10px', top: '22px', fontSize: '16px', fontFamily: 'sans-serif' }} >
+            Cursus iaculis etiam in In nullam donec sem sed <br /> consequat scelerisque nibh ...
+          </p>
+        <div
+          style={{ position: 'relative', left: '10px', top: '-50px', fontSize: '13px', fontFamily: 'sans-serif' }}
+        >
+          <span >akbarh   </span>
+          <span>  July 7, 2021</span>
+        </div>
+      </div>
+
+
+      <div className="relative w-1/3 mx-4">
+     
+     <img
+       src={'https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/hero01-free-img-1024x647.jpg'}
+       alt=""
+       className="w-full h-64 object-cover"
+       style={{ position: 'relative', top: '-25px' }}
+     />
+       <p style={{ left: '10px', top: '-8px', position: 'relative', fontSize: '21px', fontFamily: 'sans-serif', fontWeight: 'bold' }}>
+       What Moves Gravel-Size Gypsum Crystals Around the Desert?
+       </p>
+       
+       <p style={{ position: 'relative', left: '10px', top: '22px', fontSize: '16px', fontFamily: 'sans-serif' }} >
+         Cursus iaculis etiam in In nullam donec sem sed <br /> consequat scelerisque nibh ...
+       </p>
+     <div
+       style={{ position: 'relative', left: '10px', top: '-50px', fontSize: '13px', fontFamily: 'sans-serif' }}
+     >
+       <span >akbarh   </span>
+       <span>  July 7, 2021</span>
+     </div>
+   </div>
+
+   <div className="relative w-1/3 mx-4">
+     
+     <img
+       src={'https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/sofa-with-pot.jpg'}
+       alt=""
+       className="w-full h-64 object-cover"
+       style={{ position: 'relative', top: '-25px' }}
+     />
+       <p style={{ left: '10px', top: '-8px', position: 'relative', fontSize: '21px', fontFamily: 'sans-serif', fontWeight: 'bold' }}>
+       What Moves Gravel-Size Gypsum Crystals Around the Desert?
+       </p>
+       
+       <p style={{ position: 'relative', left: '10px', top: '22px', fontSize: '16px', fontFamily: 'sans-serif' }} >
+         Cursus iaculis etiam in In nullam donec sem sed <br /> consequat scelerisque nibh ...
+       </p>
+     <div
+       style={{ position: 'relative', left: '10px', top: '-50px', fontSize: '13px', fontFamily: 'sans-serif' }}
+     >
+       <span >akbarh   </span>
+       <span>  July 7, 2021</span>
+     </div>
+   </div>
+    </div>
+    </div>
+   </div>
+    )
+}
+
+export default BlogPosts;
