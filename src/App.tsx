2dbbc0d07ae39aec292889068b1349607571f159
ADJE2004 <kemiadje@gmail.com>
2024-12-06 23:31:22 +0100

    la page contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import Interiorlogo4freeimg from './assets/Interior-logo4-free-img.png';
 import './App.css';
-import Map from "./components/map/map";
+
+import Aboutus from "./Components/ContactUs/Aboutus"
 
 
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
 
 
 function App() {
   return (
-    <div className="App">
-    
-     
-     <Header/>
-     <div className='main'>
-     
-     </div>
-     <Map/>
-     <Footer/>
+    <div>
+      <Aboutus/>
     </div>
   );
 }
---
+++ src/Components/ContactUs/Aboutus.tsx
@@ -0,0 +1,20 @@
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Map from './map/map';
+
+function Aboutus() {
+    return (
+      <div className="App">
+      
+       
+       <Header/>
+       <div className='main'>
+       
+       </div>
+       <Map/>
+       <Footer/>
+      </div>
+    );
+  }
+  
+  export default Aboutus;
